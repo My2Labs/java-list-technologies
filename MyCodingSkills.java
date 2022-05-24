f94356a8594b1fa77990906bc3a01df53585095f
My2Labs <sking171@ford.com>
2022-05-24 12:15:02 -0500

    Work exercise.

---
+++ MyCodingSkills.java
@@ -0,0 +1,12 @@
+class MyCodingSkills {
+    public static void main(String args[]) {
+        System.out.println("CSS");
+        System.out.println("HTML");
+        System.out.println("JavaScript");
+        System.out.println("TypeScript");
+        System.out.println("Java");
+        System.out.println("OOP");
+        System.out.println("SQL");
+        System.out.println("Spring Boot");
+    }
+}
